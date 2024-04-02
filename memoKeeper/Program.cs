7be077cea77a39411c1b2af4fd345f7931f51ca3
Stooge <batman.adamwes@gmail.com>
2024-04-02 17:05:06 -0400

    created project

---
+++ memoKeeper/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace memoKeeper;
+
+class Program
+{
+
+    //start with Menu UI
+        //Menu:
+            //View memo list
+            //  |-->select/view
+            //      |-->edit-->save
+            //      |-->delete
+            //View memo by name
+            //  |-->take input-->display memo
+            //      |-->edit-->save
+            //      |-->delete
+            //Create new Memo
+            //  |-->save
+            //  |-->discard
+            //Exit(save changes)
+            
+
+}
