578b5b5c2c51f97ddc1a123daf8acd5207f9b859
Stooge <batman.adamwes@gmail.com>
2024-04-11 09:38:48 -0400

    added then removed db connection through efcore

--- memoKeeper/memoKeeperApp/obj/Debug/net8.0/memoKeeper.AssemblyInfo.cs
+++ memoKeeper/memoKeeperApp/obj/Debug/net8.0/memoKeeper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("memoKeeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2925db053ab2ee0c57f10c3d78739fdcd7fad095")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9260d1318fe0ac8aa7d7aaeda8c5969e9005e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("memoKeeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("memoKeeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
